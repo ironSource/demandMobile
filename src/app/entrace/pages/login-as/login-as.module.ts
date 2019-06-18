3d344cf2f1eecb2b3b96c4dc672a74cf9bcb54b2
chen.levin <chen.levin@ironsrc.com>
2019-06-18 16:13:29 +0300

    add support for auth

---
+++ src/app/entrace/pages/login-as/login-as.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { LoginAsPage } from './login-as.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: LoginAsPage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [LoginAsPage]
+})
+export class LoginAsPageModule {}
---
+++ src/app/entrace/pages/login-as/login-as.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login-as',
+  templateUrl: './login-as.page.html',
+  styleUrls: ['./login-as.page.scss'],
+})
+export class LoginAsPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
